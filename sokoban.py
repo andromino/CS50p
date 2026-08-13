2f7cfbdc34c1bb157368907aa2f61888bf9605cc
Hunter <androminoza@gmail.com>
2026-08-13 16:56:13 +0200

    Update sokoban

--- sokoban.py
+++ sokoban.py
@@ -2,10 +2,24 @@ def main():
     history = []
 
     while True:
-        action = input("Action: ")
-        history.append(action)
-        print(history)
+        action = input("Action: ").lower()
         
+        if action == "undo":
+            undone = history.pop()
+            print(f"Undone: {undone}")
+            continue
+        elif action == "quit":
+            print("Bye!")
+            break
+        elif action == "restart":
+            history.clear()
+            print("Restarted!")
+            continue
+        else:
+            history.append(action)
+
+        print(history)
+     
 
 
 main()
